2bebc07eb8305e03d5d8a5e5a96c2956cb4d9527
DarkShy <30442729+DarkShyMW@users.noreply.github.com>
2023-04-28 00:59:11 +0300

    Update i.py

--- i.py
+++ i.py
@@ -1,26 +1,34 @@
+"""
+This script generates random images with triangles and uploads them to Mastodon.
+"""
+
 import os
 import time
 import numpy as np
 from mastodon import Mastodon
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image, ImageDraw
 
-# Ваши данные доступа к Mastodon
-client_id = 'CLIENT_ID'
-client_secret = 'CLIENT_SECRET'
-access_token = 'ACCESS_TOKEN'
-base_url = 'https://mstdn.bronyfurry.com'
+# Your Mastodon API credentials
+CLIENT_ID = 'CLIENT_ID'
+CLIENT_SECRET = 'CLIENT_SECRET'
+ACCESS_TOKEN = 'ACCESS_TOKEN'
+API_BASE_URL = 'https://mstdn.bronyfurry.com'
 
 mastodon = Mastodon(
-    client_id=client_id,
-    client_secret=client_secret,
-    access_token=access_token,
-    api_base_url=base_url
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    access_token=ACCESS_TOKEN,
+    api_base_url=API_BASE_URL,
 )
 
-num_triangles = 5
-size = 512
+NUM_TRIANGLES = 5
+SIZE = 512
+
 
 def generate_image_with_colors(num_triangles, size, colors):
+    """
+    Generates a PIL image with random triangles of random colors.
+    """
     image = Image.new('RGB', (size, size), color=colors[0])
     draw = ImageDraw.Draw(image)
 
@@ -28,7 +36,7 @@ def generate_image_with_colors(num_triangles, size, colors):
         vertices = [
             (np.random.randint(0, size), np.random.randint(0, size)),
             (np.random.randint(0, size), np.random.randint(0, size)),
-            (np.random.randint(0, size), np.random.randint(0, size))
+            (np.random.randint(0, size), np.random.randint(0, size)),
         ]
         draw.polygon(vertices, fill=colors[np.random.randint(0, len(colors))])
 
@@ -36,18 +44,28 @@ def generate_image_with_colors(num_triangles, size, colors):
 
 
 def create_and_upload_image_by_request(num_triangles, size, colors, mastodon):
+    """
+    Generates an image and uploads it to Mastodon.
+    """
     image = generate_image_with_colors(num_triangles, size, colors)
     image_path = 'image.png'
     image.save(image_path)
+
     media_dict = mastodon.media_post(image_path)
     mastodon.status_post(status='Here is your image!', media_ids=[media_dict['id']])
+
     os.remove(image_path)
 
 
 def main():
+    """
+    Main function that generates and uploads images every 15 minutes.
+    """
     while True:
-        create_and_upload_image_by_request(num_triangles, size, [tuple(np.random.randint(0, 256, size=3)) for i in range(3)], mastodon)
-        time.sleep(9000) # 15 минут
+        colors = [tuple(np.random.randint(0, 256, size=3)) for i in range(3)]
+        create_and_upload_image_by_request(NUM_TRIANGLES, SIZE, colors, mastodon)
+        time.sleep(900)  # 15 minutes
+
 
 if __name__ == '__main__':
     main()
